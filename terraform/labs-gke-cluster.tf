55054d311d63e2e08e6b36171d46856551855a5c
Michael Sobota <mjsobota@gmail.com>
2019-05-24 23:17:22 -0600

    cluster default scale down

--- terraform/labs-gke-cluster.tf
+++ terraform/labs-gke-cluster.tf
@@ -31,7 +31,7 @@ resource "google_container_node_pool" "primary_preemptible_nodes" {
   location   = "us-central1"
   project    = "alien-gadget-219904"
   cluster    = "${google_container_cluster.primary.name}"
-  node_count = 2
+  node_count = 1
 
   node_config {
     preemptible  = true
